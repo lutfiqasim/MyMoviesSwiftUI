137821f98ccbb2e41770d943535a012335614a6b
lqasim <l.qasim@homyt.net>
2025-11-16 21:24:11 +0200

    Initial Commit

---
+++ MyMoviesSwiftUI/MyMoviesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMoviesSwiftUIApp.swift
+//  MyMoviesSwiftUI
+//
+//  Created by Lutfi Qasim on 16/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct MyMoviesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
